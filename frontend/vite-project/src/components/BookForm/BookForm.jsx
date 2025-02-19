cb1d74e1f3909e42c096446951aa0d5173b80aa8
Nikita Zinich <nikitazinich4@gmail.com>
2025-02-19 16:24:06 +0600

    Added isLoading and spinner in the BookForm

--- frontend/vite-project/src/components/BookForm/BookForm.jsx
+++ frontend/vite-project/src/components/BookForm/BookForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./BookForm.css";
+import { FaSpinner } from "react-icons/fa";
 import booksData from "../../data/books.json";
 import createBookWuthID from "../../utils/creatBookWithID";
 import { setError } from "../../redux/slices/errorSlice";
@@ -10,6 +11,8 @@ import { useDispatch } from "react-redux";
 export default function BookForm() {
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
   const dispatch = useDispatch();
 
   const handleAddRandomBook = () => {
@@ -30,8 +33,13 @@ export default function BookForm() {
     }
   }
 
-  const handleAddRandomBookByAPI = () => {
-    dispatch(fetchBook("http://localhost:4000/random-book"));
+  const handleAddRandomBookByAPI = async () => {
+    try {
+      setIsLoading(true);
+      await dispatch(fetchBook("http://localhost:4000/random-book-delay"));
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -60,8 +68,20 @@ export default function BookForm() {
         <button type="button" onClick={handleAddRandomBook}>
           Add random
         </button>
-        <button type="button" onClick={handleAddRandomBookByAPI}>
-          Add random book by API
+
+        <button
+          type="button"
+          onClick={handleAddRandomBookByAPI}
+          disabled={isLoading}
+        >
+          {isLoading ? (
+            <>
+              <span>Loading Book...</span>
+              <FaSpinner className="spinner" />
+            </>
+          ) : (
+            "Add random book by API"
+          )}
         </button>
       </form>
     </div>
