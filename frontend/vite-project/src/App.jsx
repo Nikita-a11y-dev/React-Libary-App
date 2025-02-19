49dbe273310b8c10add546264a07125c1e027189
Nikita Zinich <nikitazinich4@gmail.com>
2025-02-19 10:48:28 +0600

    Created API service

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <div className="app">
       <header className="app-header">
-        <h1>Book Libery AApp</h1>
+        <h1>Book Libery App</h1>
       </header>
       <main className="app-main">
         <div className="app-left-column">
